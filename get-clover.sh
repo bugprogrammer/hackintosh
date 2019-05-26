bdf2a07806c6949030e7e9a52831750aac57969a
bugprogrammer <87811991@163.com>
2019-05-26 15:43:58 +0800

    shell update

--- get-clover.sh
+++ get-clover.sh
@@ -2,12 +2,28 @@
 
 clear
 echo '脚本开始执行。'
+echo '正在进行环境检测。'
 
+os=$(uname -s)
+if [ ${os} == 'Linux' ]; then
+	echo '检测到您的操作系统是Linux(只支持ubuntu)'
+    if ! [ -x "$(command -v git)" ]; then
+  		echo '尚未安装git,将自动安装'
+  		sudo apt-get install git
+	fi
+
+	if ! [ -x "$(command -v nautilus)" ]; then
+  		echo '尚未安装nautilus,将自动安装'
+  		sudo apt-get install nautilus
+	fi
+elif [ ${os} == 'Darwin' ]; then
+	echo '您的操作系统是macOS'
+fi
 echo -e '\n正在获取Bugprogrammer的Hackintosh仓库，请稍后:'
 echo '-------------------------------------'
 
 cd ~
-
+#dir='hackintosh_'${date '+%Y%m%d%H%M%S'}
 dir=hackintosh_$(date +%Y%m%d%H%M%S)
 git clone https://github.com/bugprogrammer/hackintosh.git ${dir}
 
@@ -25,4 +41,8 @@ echo '您选择的机型为:'${type[$input-1]}',现在开始下载Clover'
 git checkout ${type[$input-1]}
 echo 'Clover下载完成，将自动打开，路径位于'
 pwd
-open .
+if [ ${os} == 'Linux' ]; then
+	nautilus .
+elif [ ${os} == 'Darwin' ]; then
+	open .
+fi
