e716690357faaf8f9b188c75a91da78a4c00b3c2
bugprogrammer <87811991@163.com>
2019-05-27 15:33:18 +0800

    shell debug

--- get-clover.sh
+++ get-clover.sh
@@ -5,7 +5,7 @@ echo '脚本开始执行。'
 echo '正在进行环境检测。'
 
 os=$(uname -s)
-if [ ${os} == 'Linux' ]; then
+if [ $os == 'Linux' ]; then
 	echo '检测到您的操作系统是Linux(只支持ubuntu)'
     if ! [ -x "$(command -v git)" ]; then
   		echo '尚未安装git,将自动安装'
@@ -16,7 +16,7 @@ if [ ${os} == 'Linux' ]; then
   		echo '尚未安装nautilus,将自动安装'
   		sudo apt-get -y install nautilus
 	fi
-elif [ ${os} == 'Darwin' ]; then
+elif [ $os == 'Darwin' ]; then
 	echo '您的操作系统是macOS'
 	xcode-select --install
 	echo -e "\033[32m请注意您的电脑是否出现弹窗，如出现弹窗，请点击最右侧的安装按钮，安装结束后按回车继续。如果未出现弹窗直接按回车继续: \033[0m"
@@ -45,8 +45,8 @@ echo '您选择的机型为:'${type[$input-1]}',现在开始下载Clover'
 git checkout ${type[$input-1]}
 echo 'Clover下载完成，将自动打开，路径位于'
 pwd
-if [ ${os} == 'Linux' ]; then
+if [ $os == 'Linux' ]; then
 	nautilus .
-elif [ ${os} == 'Darwin' ]; then
+elif [ $os == 'Darwin' ]; then
 	open .
 fi
